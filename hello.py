7d1e548fcde4603fd19a02ade86517ef4efda5e4
coleochieng <nicole.ochieng98@gmail.com>
2023-03-25 23:10:15 -0500

    use string interpolation

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 msg = 'I''m so glad I made it this far into coding today'
 name = 'Nicole'
 print('Hey yo ' + name + ' ' + msg)
+
+name = 'Nicole'
+age = 24
+msg = f'{name} is {age} years old'
+print(msg)
+
+template = "I was {} years old when I {}"
+print ( template.format(17, 'got viral meningitis'))
