118dd54fa4fe456d72eb1cbadc6d76221468225c
coleochieng <nicole.ochieng98@gmail.com>
2023-03-25 23:32:26 -0500

    use methods on strings

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
 msg = 'I''m so glad I made it this far into coding today'
 name = 'Nicole'
-print('Hey yo ' + name + ' ' + msg)
+#print('Hey yo ' + name + ' ' + msg)
 
-name = 'Nicole'
 age = 24
 msg = f'{name} is {age} years old'
-print(msg)
+#print(msg)
 
 template = "I was {} years old when I {}"
-print ( template.format(17, 'got viral meningitis'))
+#print ( template.format(17, 'got viral meningitis'))
+
+name = 'nicole'
+scream_name = name.upper()
+print(scream_name)
+
+expectation = 'We regret to inform you'
+reality = expectation.replace('regret', 'are pleased')
+print(reality)
+
+boo = 'eggs' in 'where are the eggs'
+print(boo)
+
