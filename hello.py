814e21b821a225ea6b0601828d04cf3f6c8d9444
coleochieng <nicole.ochieng98@gmail.com>
2023-03-26 20:06:02 -0500

    make if/elif/else staement

--- hello.py
+++ hello.py
@@ -1,22 +1,21 @@
-msg = 'I''m so glad I made it this far into coding today'
-name = 'Nicole'
-#print('Hey yo ' + name + ' ' + msg)
+#control flow
+color = input('Enter "green", "yellow", or "red": ').lower()
+print(f'The user has enetered {color}')
 
-age = 24
-msg = f'{name} is {age} years old'
-#print(msg)
+if color == 'green':
+    print('Go!')
+    color = input('Enter "green", "yellow", or "red": ').lower()
+elif color == 'yellow':
+    print('Slow down!')
+    color = input('Enter "green", "yellow", or "red": ').lower()
+elif color == 'red':
+    print('Stop!')
+    color = input('Enter "green", "yellow", or "red": ').lower()
+else:
+    print('Chloe... follow instruction')
+    color = input('Enter "green", "yellow", or "red": ').lower()
 
-template = "I was {} years old when I {}"
-#print ( template.format(17, 'got viral meningitis'))
 
-name = 'nicole'
-scream_name = name.upper()
-print(scream_name)
 
-expectation = 'We regret to inform you'
-reality = expectation.replace('regret', 'are pleased')
-print(reality)
 
-boo = 'eggs' in 'where are the eggs'
-print(boo)
 
