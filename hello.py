3faa7c27f43347fb61d369cef34f33ff2542d469
coleochieng <nicole.ochieng98@gmail.com>
2023-03-26 20:34:36 -0500

    use while loop to continue prompts

--- hello.py
+++ hello.py
@@ -2,18 +2,22 @@
 color = input('Enter "green", "yellow", or "red": ').lower()
 print(f'The user has enetered {color}')
 
-if color == 'green':
-    print('Go!')
-    color = input('Enter "green", "yellow", or "red": ').lower()
-elif color == 'yellow':
-    print('Slow down!')
-    color = input('Enter "green", "yellow", or "red": ').lower()
-elif color == 'red':
-    print('Stop!')
-    color = input('Enter "green", "yellow", or "red": ').lower()
-else:
-    print('Chloe... follow instruction')
-    color = input('Enter "green", "yellow", or "red": ').lower()
+while color != 'quit':
+    if color == 'green':
+        print('Go!')
+        color = input('Enter "green", "yellow", "red": ').lower()
+    elif color == 'yellow':
+        print('Slow down!')
+        color = input('Enter "green", "yellow", "red": ').lower()
+    elif color == 'red':
+        print('Stop!')
+        color = input('Enter "green", "yellow", "red": ').lower()
+    else:
+        print('Bogus!')
+        color = input('Enter "green", "yellow", "red": ').lower()
+
+if color == 'quit':
+    print('The user has quit')
 
 
 
