d14326c609b1bde80e1e04cd908c116c9b84a858
coleochieng <nicole.ochieng98@gmail.com>
2023-03-28 00:35:05 -0500

    use for/in to iterate over multiple dictionaries in a list

--- containers.py
+++ containers.py
@@ -10,7 +10,7 @@ print(name)
 
 student['name'] = 'Olivia'
 print(student['name'])
-'''
+
 
 if 'school' in student:
     print( f"{student['name']} goes to {student['school']}")
@@ -37,6 +37,32 @@ print(all)
 
 for key, val in student.items():
     print(f'{key} ={val}')
+
+where_my_things_are = {
+    'phone': 'fanny pack',
+    'glasses': 'bedside table',
+    'computer': 'bookbag',
+    'lotion': 'bathroom'
+}
+
+for key, val in where_my_things_are.items():
+    print(f'My {key} is kept {val}')
+
+'''
+
+scores = [
+    {
+     'name': 'Bill',
+     'points': 25
+    },
+    {
+     'name': 'Bob',
+     'points': 30
+    }
+]
+
+for score in scores:
+    print(f"{score['name']} scored {score['points']} points")
 
 
 
