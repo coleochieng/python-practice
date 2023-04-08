fe74b0633648350fd6ed5d40e05f19a42611fac5
coleochieng <nicole.ochieng98@gmail.com>
2023-04-07 23:47:43 -0500

    use for loop with *args

--- functions.py
+++ functions.py
@@ -4,8 +4,18 @@ def first_function():
 
 result = first_function
 print(result)
-'''
+
 def add(a, b):
     return a + b
 
-add()
+print(add(6, 3))
+'''
+
+def artist_work(artist_name, *args):
+    artist = {'name': artist_name, 'songs': []}
+    for song in args:
+        artist['songs'].append(song)
+    return artist
+
+print( artist_work('Radiohead', 'Nude', 'Separator', 'Little by Little', 'Lotus Flower') )
+
