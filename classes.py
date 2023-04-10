14c0d72c21ff51f2d366fca5281358052f94ab7a
coleochieng <nicole.ochieng98@gmail.com>
2023-04-10 14:40:01 -0500

    make a class with attributes and methods and dunder methods

--- classes.py
+++ classes.py
@@ -1,10 +1,33 @@
+#class example
+'''
+class Dog():
+  def __init__(self, name, age=0):
+    self.name = name
+    self.age = age
+
+  def bark(self):
+    print(f'{self.name} says woof!')
+
+'''
+
 class Vehicle():
     def __init__(self, vin, make, model, running=False):
         self.vin = vin
         self.make = make
         self.model = model
         self.running = running
-    def start():
-        running = True
-    def stop():
-        running = False
+    def start(self):
+        self.running = True
+        return self.running
+    def stop(self):
+        self.running = False
+        return self.running
+    
+car = Vehicle('TS123', 'Tesla', 'Model S')
+print(car.running) # -> False
+car.start()
+print(car.running) # -> True
+plane = Vehicle('X99Y', 'Boeing', '747-B')
+print(plane.vin, plane.make, plane.model)
+
+
